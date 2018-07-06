c77db05288e5e1bb706cd020ff66a95d51b22b65
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-07-06 20:11:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_a_grandma(string)
   if string.upcase
-    
+    NO, NOT SINCE 1938!
+  elsif string.lower
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
