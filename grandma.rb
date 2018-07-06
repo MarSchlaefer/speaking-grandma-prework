493360dab9b2f7f6a2d258e2136bb0396d272d58
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-07-06 20:18:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,4 @@
-def speak_to_a_grandma(string)
-  if string == string.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif string == string.downcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  end
-end
+def 
 
 
 # Whatever you say to grandma, she should respond with
