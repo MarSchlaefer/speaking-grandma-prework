f3982109a33addeccb3fc23f35356aecf45190de
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-07-06 20:24:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
-  elsif string == string.capitalize
+  else 
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
